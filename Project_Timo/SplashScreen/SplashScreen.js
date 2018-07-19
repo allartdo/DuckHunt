b6a8351174f3cba17cd936b4facc54a700c6ec1b
TimoBrandt1 <timobrandt2002@gmail.com>
2018-07-19 21:59:10 +0200

    weer een fail

--- Project_Timo/SplashScreen/SplashScreen.js
+++ Project_Timo/SplashScreen/SplashScreen.js
@@ -1,2 +1,11 @@
-
-
+var shotDelay = document.getElementById('Schot')
+function shotDelay(){
+    setTimout(shotDelay, 9500)
+    
+}
+//de functie schot moet activeren na 9,5 seconden dus hij telt tot 9,5 seconden als hij boven de 9,5 seconden is moet het sschot afgaan.
+//Secondes tellen
+//Na zoveel seconde afgaan
+//Hij telt 
+// Javascript timer zoeken.
+//
