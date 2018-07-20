d0bb725f932a5cd48945590ec2ac823dd49cbccf
allartdo <allartdo@gmail.com>
2018-07-20 11:55:26 +0200

    nu werkt hij zoals hij hoort
    a

--- Project_Allart/Audio ckecker/audioChecker.js
+++ Project_Allart/Audio ckecker/audioChecker.js
@@ -1,7 +1,11 @@
+document.getElementById('backGroundSound').innerHTML = '<iframe class="testFrame" id="Schot" src="./rust.mp3"></iframe>';
+document.getElementById('backGroundSound').innerHTML = false;
+
+
 var audio = document.getElementById('Rust');
 var onOff = document.getElementById('settingsTextSwitch');
 var musicClick = 0;
-
+audio.volume = '0.1';
 function switchMusic () {
     if (musicClick == 0) {
         audio.pause();
