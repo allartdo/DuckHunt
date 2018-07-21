0bc961617eb46062b7fd5125d841735929858455
TimoBrandt1 <timobrandt2002@gmail.com>
2018-07-21 17:41:49 +0200

    eindelijk gelukt :DDD

--- Project_Timo/SplashScreen/SplashScreen.js
+++ Project_Timo/SplashScreen/SplashScreen.js
@@ -1,6 +1,9 @@
 var settingsClick = 0;
 var musicClick = 0;
 var shopClick = 0;
+var shopBack = 0;
+var levelClick = 0;
+var levelsBack = 0;
 var audio = document.getElementById('Rust');
 var onOff = document.getElementById('settingsTextSwitch');
 
@@ -37,10 +40,33 @@ function switchMusic () {
 function toShop() {
     if (shopClick == 0) {
         document.getElementById('mmMenu').style.display = 'none';
-        shopClick = 1;
+        document.getElementById('duckTitle').style.display = 'none';
+        document.getElementById('shopPage').style.display = 'block';
+        document.getElementById('shopTitle').style.display = 'block';
+        document.getElementById('shopBack').style.display = 'block';
+    } 
+    
+}
+function shopBackToMenu() {
+    if (shopBack == 0) {
+        document.getElementById('mmMenu').style.display = 'block';
+        document.getElementById('duckTitle').style.display = 'block';
+        document.getElementById('shopPage').style.display = 'none';
     }
-    else if(shopClick == 1) {
+}
+function toLevels() {
+    if(levelClick == 0) {
         document.getElementById('mmMenu').style.display = 'none';
-        shopClick = 0;
+        document.getElementById('duckTitle').style.display = 'none';
+        document.getElementById('levelsPage').style.display = 'block';
+        document.getElementById('levelsTitle').style.display = 'block';
+        document.getElementById('levelsBack').style.display = 'block';
+    }
+}
+function levelsBackToMenu() {
+    if(levelsBack == 0) {
+        document.getElementById('mmMenu').style.display = 'block';
+        document.getElementById('duckTitle').style.display = 'block';
+        document.getElementById('levelsPage').style.display = 'none';
     }
 }
