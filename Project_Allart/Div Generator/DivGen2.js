90376749cfce0bf366ed68fed9fca11f38b42de3
allartdo <allartdo@gmail.com>
2018-07-31 13:01:30 +0200

    w

--- Project_Allart/Div Generator/DivGen2.js
+++ Project_Allart/Div Generator/DivGen2.js
@@ -1,9 +1,22 @@
-var guns = 'sniper, smg, gun, pistol, shotgun, pistol2, pistol3, pistol4, pistol5'.split(',');
-for (var c in guns) {
-    var gun = document.createElement('div');
-    gun.id = guns[c];
-    gun.className = "guns";
-    gun.innerHTML = guns[c];
-    document.getElementById("Container").appendChild(gun);
+var pistols = 'pistol1, pistol2, pistol3, pistol4, pistol5, pistol6, pistol7, sniper1, sniper2, sniper3, sniper4, sniper5, sniper6, sniper7'.split(',');
+for (var p in pistols) {
+    var pistol = document.createElement('div');
+    pistol.id = pistols[p];
+    pistol.className = "pistols";
+    pistol.innerHTML = pistols[p];
+    document.getElementById("Container").appendChild(pistol);
+    console.log(pistol.id);
+    if (pistol > 7) {
+        
+    }
 }
 
+
+/*for (var s in snipers) {
+    sniper.id = sniper[s];
+    sniper.className = "pistols";
+    sniper.innerHTML = sniper[s];
+    document.getElementById("Container").appendChild(pistol);
+    document.getElementById("Container").appendChild(sniper);
+}
+*/
